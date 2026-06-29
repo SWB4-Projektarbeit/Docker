4d9b12714ebbf5d5b8db9f184de2c6aaa14069ec
Zeanon <thezeanon@gmail.com>
2026-06-29 16:35:15 +0200

    leave frontend and backend in their own repos

--- Backend/Backend/src/main/java/de/hsesslingen/timesy/backend/dto/BuildingDTO.java
+++
@@ -1,9 +0,0 @@
-package de.hsesslingen.timesy.backend.dto;
-
-import lombok.NonNull;
-
-import java.util.List;
-
-public record BuildingDTO(@NonNull String buildingName,
-						  @NonNull List<RoomDTO> rooms) {
-}

61dd4ed5661cc26574b7d14d81c5c187d1cfd4d7
Zeanon <thezeanon@gmail.com>
2026-06-29 16:38:45 +0200

    fix

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git clone https://github.com/SWB4-Projektarbeit/Frontend.git
-git clone https://github.com/SWB4-Projektarbeit/Backend.git
+git clone https://github.com/SWB4-Projektarbeit/Frontend.git Frontend/source
+git clone https://github.com/SWB4-Projektarbeit/Backend.git Backend/source

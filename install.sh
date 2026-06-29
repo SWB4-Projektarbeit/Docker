c10a0ce181324319db096829832e0b6b962cd459
Zeanon <thezeanon@gmail.com>
2026-06-29 17:10:09 +0200

    fix

--- install.sh
+++ install.sh
@@ -2,3 +2,9 @@
 
 git clone https://github.com/SWB4-Projektarbeit/Frontend.git Frontend/source
 git clone https://github.com/SWB4-Projektarbeit/Backend.git Backend/source
+
+cp Backend/source/src/main/resources/application.properties.default Backend/source/src/main/resources/application.properties
+
+if [ ! -f .env ]; then
+    cp .env.default .env
+fi
